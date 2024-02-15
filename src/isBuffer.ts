69087071d592467e5cb6be6035d7e365d93f656e
trevor-anderson <trevor@nerdware.cloud>
2024-02-15 14:10:55 -0500

    fix: replace any-param w unknown in isBuffer

--- src/isBuffer.ts
+++ src/isBuffer.ts
@@ -1,10 +1,18 @@
 import { root } from "./.internal/root.js";
 
+/**
+ * `Buffer.isBuffer` doesn't throw if an argument is not provided, so this
+ * type is used instead of `BufferConstructor["isBuffer"]` in order to
+ *     1. replace `any` with `unknown`, and
+ *     2. better reflect the behavior of the function.
+ */
+export type IsBufferWithOptionalArgument = (value?: unknown) => value is Buffer;
+
 /** NodeJS `Buffer.isBuffer`, if available. */
-const nativeIsBuffer = root?.Buffer?.isBuffer as BufferConstructor["isBuffer"] | undefined;
+const nativeIsBuffer = root?.Buffer?.isBuffer as IsBufferWithOptionalArgument | undefined;
 
 /** no-op if `Buffer.isBuffer` is not available. */
-const noOpIsBuffer = (() => false) as unknown as BufferConstructor["isBuffer"];
+const noOpIsBuffer = (() => false) as IsBufferWithOptionalArgument;
 
 /**
  * Checks if `value` is a buffer.
@@ -19,5 +27,5 @@ const noOpIsBuffer = (() => false) as unknown as BufferConstructor["isBuffer"];
  * isBuffer(new Uint8Array(2)) // => false
  * ```
  */
-export const isBuffer: BufferConstructor["isBuffer"] =
+export const isBuffer: IsBufferWithOptionalArgument =
   typeof nativeIsBuffer === "function" ? nativeIsBuffer : noOpIsBuffer;
