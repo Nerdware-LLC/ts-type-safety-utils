51801f4010612eb4d99eb6104b54f5c6481a8480
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-02-24 05:34:17 -0500

    refactor: extract jsdoc types to TS type land

--- src/.internal/getTag.ts
+++ src/.internal/getTag.ts
@@ -2,10 +2,10 @@
  * Gets the `toStringTag` of `value`.
  *
  * @internal
- * @param {*} value The value to query.
- * @returns {string} Returns the `toStringTag`.
+ * @param value - The value to query.
+ * @returns The `toStringTag`.
  */
-export const getTag = (value?: unknown) => {
+export const getTag = (value?: unknown): string => {
   /* The loose-equality check below (`value == null`) will return `true` for
   both `null` and `undefined`, thereby ensuring only 1 comparison is needed
   to check for both `null` and `undefined`. */
