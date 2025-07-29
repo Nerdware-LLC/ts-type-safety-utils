d32805aa9c5de8ac42362d07b937ed0de1161d4d
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2025-07-29 12:29:52 -0400

    style: update code to reflect new prettier fmt for ops

--- src/.internal/root.ts
+++ src/.internal/root.ts
@@ -2,24 +2,24 @@ import { freeGlobal } from "./freeGlobal.js";
 
 // Detect free variable `globalThis`:
 const freeGlobalThis =
-  typeof globalThis === "object" &&
-  globalThis !== null && // eslint-disable-line @typescript-eslint/no-unnecessary-condition
-  globalThis.Object === Object &&
-  globalThis;
+  typeof globalThis === "object"
+  && globalThis !== null // eslint-disable-line @typescript-eslint/no-unnecessary-condition
+  && globalThis.Object === Object
+  && globalThis;
 
 // Detect free variable `self`:
 const freeSelf =
-  typeof self === "object" &&
-  self !== null && // eslint-disable-line @typescript-eslint/no-unnecessary-condition
-  self.Object === Object &&
-  self;
+  typeof self === "object"
+  && self !== null // eslint-disable-line @typescript-eslint/no-unnecessary-condition
+  && self.Object === Object
+  && self;
 
 /**
  * Used as a reference to the global object.
  * @internal
  */
 export const root =
-  freeGlobalThis || // prettier-ignore
-  freeGlobal ||
-  freeSelf ||
-  (Function("return this")() as Partial<typeof globalThis>); // eslint-disable-line @typescript-eslint/no-implied-eval,@typescript-eslint/no-unsafe-call
+  freeGlobalThis // prettier-ignore
+  || freeGlobal
+  || freeSelf
+  || (Function("return this")() as Partial<typeof globalThis>); // eslint-disable-line @typescript-eslint/no-implied-eval,@typescript-eslint/no-unsafe-call
